1406bcbae7e77ccd2cc602422135783a8823cbea
Junaem <junh9547@gmail.com>
2021-03-22 03:28:27 +0900

    21.03.21

--- backjoon_1/src/bruteforce/Q2798.java
+++ backjoon_1/src/bruteforce/Q2798.java
@@ -1,10 +1,11 @@
+package bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Q2798{
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
