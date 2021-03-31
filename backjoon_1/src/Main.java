01bab7da383da95e79124aca9814739d434ffc43
Junaem <junh9547@gmail.com>
2021-03-31 21:18:44 +0900

    21.03.31

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,48 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main{
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		String[][] arr = new String[n][2];
-		
-		for(int i=0; i<n; i++) {
-			arr[i][0] = br.readLine();
-			arr[i][1] = arr[i][0].length()+"";
-		}
-		br.close();
-		
-		Arrays.sort(arr, (e1, e2) ->{
-			if(Integer.parseInt(e1[1]) == Integer.parseInt(e2[1])) {
-				for(int i=0; i<Integer.parseInt(e1[1]);i++) {
-					if(e1[0].charAt(i)!=e2[0].charAt(i))
-						return e1[0].charAt(i) - e2[0].charAt(i);
-				}
-				return -1;
-			}else {
-				return Integer.parseInt(e1[1]) - Integer.parseInt(e2[1]);
-			}
-		});
-		
-		boolean exst = false;
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 		StringBuilder sb = new StringBuilder();
 		
-		for(int i=0; i<n; i++) {
-			exst=false;
+		for(int i=0; i<m; i++) {
+			int[] arr = new int[m];
 			
-			for(int j=0; j<i; j++) {
-				if(arr[j][0].equals(arr[i][0])) {
-					exst=true;
-					break;
+			for(int j=1; j<=n; j++) {
+				boolean exst = false;
+				for(int a : arr) {
+					if(a==j)
+						exst = true;
 				}
+				if(!exst)
+					arr[i] = j;
 			}
-			if(exst==false)
-				sb.append(arr[i][0]).append('\n');
+			for(int j=0; j<m; j++)
+				sb.append(arr[i]).append(' ');
+			sb.append('\n');
 		}
-		System.out.println(sb);
+		
 	}
 }
---
+++ backjoon_1/src/sort/Q1181.java
@@ -0,0 +1,49 @@
+package sort;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q1181{
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[][] arr = new String[n][2];
+		
+		for(int i=0; i<n; i++) {
+			arr[i][0] = br.readLine();
+			arr[i][1] = arr[i][0].length()+"";
+		}
+		br.close();
+		
+		Arrays.sort(arr, (e1, e2) ->{
+			if(Integer.parseInt(e1[1]) == Integer.parseInt(e2[1])) {
+				for(int i=0; i<Integer.parseInt(e1[1]);i++) {
+					if(e1[0].charAt(i)!=e2[0].charAt(i))
+						return e1[0].charAt(i) - e2[0].charAt(i);
+				}
+				return -1;
+			}else {
+				return Integer.parseInt(e1[1]) - Integer.parseInt(e2[1]);
+			}
+		});
+		
+		boolean exst = false;
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<n; i++) {
+			exst=false;
+			
+			for(int j=0; j<i; j++) {
+				if(arr[j][0].equals(arr[i][0])) {
+					exst=true;
+					break;
+				}
+			}
+			if(exst==false)
+				sb.append(arr[i][0]).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
