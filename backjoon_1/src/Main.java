8b1562105c751906feb983b50d3fe10581c15573
Junaem <junh9547@gmail.com>
2021-04-12 16:50:29 +0900

    21.04.12

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,59 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main{
 	static int N;
-	static int[] arr;
-	static Integer[] Mdp, Pdp;
+	static int[][] arr;
+	static Integer[] dp;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		Mdp = new Integer[N];
-		Pdp = new Integer[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		arr = new int[N][2];
+		dp = new Integer[N];
+		StringTokenizer st;
 		
 		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			Mdp[i] = 1;
-			Pdp[i] = 1;
+			st = new StringTokenizer(br.readLine(), " "	);
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
+		Arrays.sort(arr, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
 		int max =0;
 		for(int i=0; i<N; i++)
 			max = Math.max(max, Calc(i));
-		System.out.println(max);
-
+		System.out.println(N-max);
 	}
+	
 	static int Calc(int whr) {
-		return mCalc(whr) + pCalc(whr) -1;
-	}
-	
-	static int mCalc(int whr) {
-		if(Mdp[whr]==1) {
-			
-			for(int i=whr-1; i>=0; i--) {
-				if(arr[i]<arr[whr]) 
-					Mdp[whr] = Math.max(Mdp[whr], mCalc(i)+1);
+		if(dp[whr]==null) {
+
+			dp[whr] = 1;
+
+			for(int i= whr+1; i<N; i++) {
+				if(arr[whr][1]< arr[i][1]) 
+					dp[whr] = Math.max(dp[whr], Calc(i) +1);
 			}
 		}
-		return Mdp[whr];
+		return dp[whr];
 	}
-	static int pCalc(int whr) {
-		if(Pdp[whr]==1) {
-			
-			for(int i=whr+1; i<N; i++) {
-				if(arr[i]<arr[whr]) 
-					Pdp[whr] = Math.max(Pdp[whr], pCalc(i)+1);
-			}
-		}
-		return Pdp[whr];
-	}
-	
-	
 }
