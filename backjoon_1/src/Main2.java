5a300e909af3d6f8960c8d4679906582fd49fb59
Junaem <junh9547@gmail.com>
2021-04-17 14:27:06 +0900

    21.04.17

--- backjoon_1/src/Main2.java
+++ backjoon_1/src/Main2.java
@@ -1,35 +1,43 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main2{
-	static StringBuilder sb;
-	static StringBuilder prt = new StringBuilder();
+	static StringBuilder sb = new StringBuilder();
 	static int N, M;
-	static ArrayList<Integer> arr = new ArrayList<>();
+	static int[] arr;
 	
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M  = sc.nextInt();
+		arr = new int[M];
+		
+		for(int i=0; i<M; i++)
+			arr[i] = 0;
 		
 		for(int i=1; i<=N; i++) {
-			sb = new StringBuilder();
 			Calc(i);
 		}
-		System.out.println(prt);
+		System.out.println(sb);
 	}
 	
 	static void Calc(int n) {
-		if(arr.size() == M) {
-			prt.append(sb).append('\n');
+		if(arr(M-1) != 0) {
+			for(int i=0; i<M; i++) {
+				sb.append(arr.get(i)).append(' ');
+			}
+			sb.append('\n');
 			return;
 		}
 		
+		arr.add(n);
 		
-		for(int i=n+1; i<N; i++) {
-		
+		for(int i=1; i<=N; i++) {
+			if(!arr.contains(i)) {
+				Calc(i);
+			}
 		}
+		arr.remove(n);
 	}
 	
 }
