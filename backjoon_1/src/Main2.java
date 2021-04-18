99b6e97894b3c5965c9c05f8e93ee0cd04038f7d
Junaem <junh9547@gmail.com>
2021-04-18 20:01:51 +0900

    21.04.18

--- backjoon_1/src/Main2.java
+++ backjoon_1/src/Main2.java
@@ -4,13 +4,14 @@ public class Main2{
 	static StringBuilder sb = new StringBuilder();
 	static int N, M;
 	static int[] arr;
-	
+	static boolean[] visit;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M  = sc.nextInt();
 		arr = new int[M];
+		visit = new boolean[N+1];
 		
 		for(int i=0; i<M; i++)
 			arr[i] = 0;
@@ -22,15 +23,13 @@ public class Main2{
 	}
 	
 	static void Calc(int n) {
-		if(arr(M-1) != 0) {
+		if(arr[M-1] != 0) {
 			for(int i=0; i<M; i++) {
-				sb.append(arr.get(i)).append(' ');
+				sb.append(arr[i]).append(' ');
 			}
 			sb.append('\n');
 			return;
 		}
-		
-		arr.add(n);
 		
 		for(int i=1; i<=N; i++) {
 			if(!arr.contains(i)) {
