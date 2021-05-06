0c6075a9321f29a179c07ba454e94b6819d699e2
Junaem <junh9547@gmail.com>
2021-05-06 18:14:17 +0900

    21.05.06

---
+++ backjoon_1/src/N_bfs/Q1697.java
@@ -0,0 +1,39 @@
+package N_bfs;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q1697{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		Queue<Integer> q = new LinkedList<>();
+		int[] dp = new int[100001];
+		
+		q.offer(N);
+		
+		while(!q.isEmpty()) {
+			int n = q.poll();
+			
+			if(n==K) {
+				System.out.println(dp[n]);
+				System.exit(0);
+			}
+			int[] arr = new int[] {n-1, n+1, n*2};
+			
+			for(int i=0; i<3; i++) {
+				if(arr[i]>=0 && arr[i]<=100000 && dp[arr[i]]==0) {
+					dp[arr[i]] = dp[n] +1;
+					q.offer(arr[i]);
+				}
+			}
+			
+			
+			
+			
+		}
+		
+	}
+}
--- backjoon_1/src/N_dynamic/Q9655.java
+++ backjoon_1/src/N_dynamic/Q9655.java
@@ -1,6 +1,7 @@
+package N_dynamic;
 import java.util.Scanner;
 
-public class Main{
+public class Q9655{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
