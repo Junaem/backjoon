5d575be21e068f8851c156d26f161e12ed74991f
Junaem <junh9547@gmail.com>
2021-05-09 21:57:02 +0900

    21.05.09

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,40 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main{
-	static int N, M;
-	static int[][] sqr, dp;
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    	N = Integer.parseInt(st.nextToken());
-    	M = Integer.parseInt(st.nextToken());
-    	sqr = new int[N][M];
-    	dp = new int[N][M];
-    	int max = 0;
-    	
-    	for(int i=0; i<N; i++) {
-    		String str = br.readLine();
-    		for(int j=0; j<M; j++) {
-    			sqr[i][j] = dp[i][j] = str.charAt(j)-'0';
-    			if(sqr[i][j]==1)
-    				max =1;
-    		}
-    	}
-    	
-    	
-    	for(int i=1; i<N; i++) {
-    		for(int j=1; j<M; j++) {
-    			if(sqr[i-1][j]==1 && sqr[i][j-1]==1 && sqr[i-1][j-1]==1) {
-    				dp[i][j] = Math.min(dp[i-1][j],
-    						Math.min(dp[i][j-1], dp[i-1][j-1]))+1;
-    				max = Math.max(max, dp[i][j]);
-    			}
-    		}
-    	}
-    	System.out.println(max*max);
-    }
-    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int [] dp = new int[N+1];
+		dp[0] = 0; 
+		dp[1] = 3;
+		dp[2] = 7;
+		
+		for(int i=3; i<=N; i++) {
+			dp[i] = dp[i-2] * 4 ;
+		}
+		System.out.println(dp[N]);
+	}
 }
