6d3c455644aa45bc453d94a945e7e01a77c9b508
Junaem <junh9547@gmail.com>
2021-05-18 23:31:57 +0900

    21.05.18

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,7 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main{
+	static ArrayList<Integer>[] arr = new ArrayList[10001];
+	static boolean[] visited = new boolean[10001];
+	static int[] ans = new int[10001];
+	static int cnt =0;
+	
 	public static void main(String[] args) {
+		sc.init();
 		
+		new Main().solve();
+	}
+	
+	static class sc{
+		static BufferedReader br;
+		static StringTokenizer st;
 		
+		static void init() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
 		
+		static String readLine() {
+			try {
+				return br.readLine();
+			} catch (IOException e) {
+			}
+			return null;
+		}
+		
+		static String readLineReplace() {
+			try {
+				return br.readLine().replaceAll("\\s+", "");
+			} catch (IOException e) {
+			}
+			return null;
+		}
+		
+		static String next() {
+			while(!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+				}
+			}
+			return st.nextToken();
+		}
+		
+		static long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		static int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		static double nextDouble()	{
+			return Double.parseDouble(next());
+		}
 	}
 }
