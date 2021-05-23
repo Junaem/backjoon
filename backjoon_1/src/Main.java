a01872595fdb3c8ff0295c4a965ab415eec33a63
Junaem <junh9547@gmail.com>
2021-05-23 19:41:26 +0900

    21.05.23

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,56 +1,29 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static int[] visit = new int[10001];
-    public static List<Integer> m[];
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        int array[] = new int[N + 1];
-        m = new ArrayList[N+1];
-        for(int i=1;i<=N;++i){
-            m[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < M; ++i) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            m[a].add(b);
-        }
-
-        for (int i=1;i<=N;++i) {
-            Arrays.fill(visit, 0);
-            if(m[i].size()!=0){
-                search(i,array);
-            }
-        }
-
-        int max = 0;
-        for (int i = 1; i <= N; ++i) {
-            if (max <= array[i]) {
-                max = array[i];
-            }
-        }
-
-        for (int i = 1; i <= N; ++i) {
-            if (array[i] == max) {
-                System.out.print(i + " ");
-            }
-        }
-
-    }
-
-    public static void search(int node, int[] array) {
-        visit[node] = 1;
-        for (int i : m[node]) {
-            if(visit[i]!=1){
-                array[i]++;
-                search(i, array);
-            }
-
-        }
-    }
+public class Main{
+	static int N, M, K;
+	static int[][] arr;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		M = Integer.valueOf(st.nextToken());
+		N = Integer.valueOf(st.nextToken());
+		K = Integer.valueOf(st.nextToken());
+		arr = new int[N+1][M+1];
+		
+		for(int i=0; i<K; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			
+			
+			
+			
+		}
+		
+	}
 }
