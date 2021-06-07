eff1d19038f43f40e159135b90bfba8f54afe5b5
Junaem <junh9547@gmail.com>
2021-06-07 23:28:56 +0900

    21.06.07

--- backjoon_1/src/Main2.java
+++ backjoon_1/src/Main2.java
@@ -1,39 +1,6 @@
-import java.util.Scanner;
-
 public class Main2{
-	static StringBuilder sb = new StringBuilder();
-	static int N, M;
-	static int[] arr;
-	static boolean[] visit;
-	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M  = sc.nextInt();
-		arr = new int[M];
-		visit = new boolean[N+1];
-		
-		Calc(0);
-		System.out.println(sb);
+		String[] a = new String[] {"adf", "dfs"};
+		System.out.println(a[0][0]);
 	}
-	
-	static void Calc(int m) {
-		if(m==M) {
-			for(int i=0; i<M; i++) {
-				sb.append(arr[i]).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-		
-		for(int i=1; i<=N; i++) {
-			if(visit[i]==false) {
-				visit[i] = true;
-				arr[m] = i;
-				Calc(m+1);
-				visit[i] = false;
-			}
-		}
-	}
-	
 }
