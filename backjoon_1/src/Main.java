885c048e58eb0633b611e14153a683634f5d71b5
Junaem <junh9547@gmail.com>
2021-06-15 03:12:42 +0900

    21.06.15

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,21 +1,19 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main{
 	static int N; 
-	static int[][] node, dp;
-	static boolean nvisit[][];
+	static List<node>[] tree;
+	static boolean visit[][];
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		node = new int[N+1][N+1];
-		nvisit = new boolean[N+1][N+1];
+		visit = new boolean[N+1][N+1];
 		StringTokenizer st;
 		
 		for(int i=1; i<N; i++) {
@@ -23,49 +21,17 @@ public class Main{
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			node[a][b]= c;
-			node[b][a] = c;
-			nvisit[a][b] = nvisit[b][a] = true;
-		}
-		
-		System.out.println(Grem());
-	}
-	
-	
-	static int Grem() {
-		int max = 0;
-		
-		for(int i=1; i<=N; i++) {
-			int con = 0;
 			
-			for(int j=1; j<=N; j++) {
-				if(node[i][j]>0)
-					con++;
-			}
-			
-			if(con>1)
-				continue;
-			max = Math.max(max, check(i));
-		}
-		return max;
-	}
-	
-	static int cur=0;
-	static int chmax=0;
-	
-	static int check(int n) {
-		for(int i=1; i<=N; i++) {
-			if(nvisit[n][i] && node[n][i]>0) {
-				nvisit[n][i] = nvisit[i][n] = false;
-				cur += node[n][i];
-				chmax = Math.max(chmax, cur);
-				check(i);
-				cur -= node[n][i];
-				nvisit[n][i] = nvisit[i][n] = true;
-			}
 		}
 		
-		return chmax;
 	}
 	
+	class node {
+		int n;
+		int w;
+		node(int n, int w){
+			this.n = n;
+			this.w = w;
+		}
+	}
 }
