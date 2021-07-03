3fbd24191eeb4fb06981b166f3f3f869ff788df8
Junaem <junh9547@gmail.com>
2021-07-03 23:02:00 +0900

    21.07.03

--- backjoon_1/src/Main.java
+++ backjoon_1/src/Main.java
@@ -1,37 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main{
-	static int N; 
-	static List<node>[] tree;
-	static boolean visit[][];
+	static int N;
+	static int[][] arr;
+	
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		N = Integer.parseInt(br.readLine());
-		visit = new boolean[N+1][N+1];
-		StringTokenizer st;
+		arr = new int[N+1][N+1];
 		
-		for(int i=1; i<N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			 
-		}
 		
-	}
-	
-	class node {
-		int n;
-		int w;
-		node(int n, int w){
-			this.n = n;
-			this.w = w;
-		}
+		
+		
+		
 	}
 }
